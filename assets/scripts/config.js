4402cf5d6843c5b9023055e0e5197cee22bfab58
thiagobadini <thiagobardini@hotmail.com>
2021-03-05 12:08:05 -0500

    Setup db

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: '<replace-with-heroku-url>',
+  production: 'https://limitless-plateau-08641.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
