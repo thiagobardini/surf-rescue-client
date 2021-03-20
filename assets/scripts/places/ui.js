07f2feb52a37dfd913a94abf7823d54c76f0b6ac
thiagobadini <thiagobardini@hotmail.com>
2021-03-20 15:53:51 -0400

    Fixed layout

--- assets/scripts/places/ui.js
+++ assets/scripts/places/ui.js
@@ -5,14 +5,13 @@ const createPlaceSuccess = (response) => {
   store.place = response.place;
   $("#showPlaces").text("Click to Refresh the Places");
   $("form").trigger("reset");
-  $('#exampleModalCenter').modal('hide')
+  $("#exampleModalCenter").modal("hide");
   $("#success-message")
     .text("Your place has been successfully created!")
     .addClass(".success");
   setTimeout(function () {
     $("#success-message").text("").addClass("success");
   }, 4000);
-
 };
 
 const createPlaceFailure = (response) => {
@@ -32,36 +31,52 @@ const showAllPlacesSuccess = (response) => {
   let storeHTML = "";
   response.places.forEach((place) => {
     storeHTML += `
-        <div class="container alert alert-primary" role="alert">
-          <a>Place ID: ${place._id}</a>
-          <p>Local Name: ${place.localName}</p>
-          <p>Country: ${place.country}</p>
-          <img src="${place.localImg}" alt="" width="auto" height="300">
-          <p>Description: ${place.description}</p>
-          <p>surfLevel: ${place.surfLevel}</p>
-          <p>Average Cost Day: US$${place.avgCostDay}</p>
-          <p>Wave Range: ${place.waveRange}ft</p>
-          <p>Great for: ${place.stance}</p>
-          <br>
-          <h4>Update your Place</h4>
-          <form class="place-update-dynamic" data-id=${place._id}>
-            <input type="text" name="place[localName]" placeholder="New name" required>
-            <input type="text" name="place[country]" placeholder="New local Name" required>
-            <input type="text" name="place[localImg]" placeholder="New localImg" required>
-            <input type="text" name="place[description]" placeholder="New description" required>
-            <input type="text" name="place[surfLevel]" placeholder="surf Level" required>
-            <input type="text" name="place[avgCostDay]" placeholder="New avgCostDay" required>
-            <input type="text" name="place[waveRange]" placeholder="New waveRange" required>
-            <input type="text" name="place[stance]" placeholder="New stance" required><br>
-            <button type="submit" class="index-dynamic-button btn btn-primary">Update Place</button>
-          </form>
-          <hr>
-          <button class='place-destroy-dynamic index-dynamic-button btn btn-primary' data-id=${place._id}>Delete Place</button>
-        </div>
-      `;
-  });
-  $("#placeList").html(storeHTML);
+    <div class="container alert alert-primary" role="alert">
+  <a>Place ID: ${place._id}</a>
+  <p>Local Name: ${place.localName}</p>
+  <p>Country: ${place.country}</p>
+  <p>Description: ${place.description}</p>
+  <p>surfLevel: ${place.surfLevel}</p>
+  <p>Average Cost Day: US$${place.avgCostDay}</p>
+  <p>Wave Range: ${place.waveRange}ft</p>
+  <br>
+  <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter2">
+    Update your Place
+  </button>
+  <button class='place-destroy-dynamic index-dynamic-button btn btn-primary' data-id=${place._id}>Delete Place</button>
 
+  <div class="modal fade" id="exampleModalCenter2" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle"
+    aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Show Id place</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-content">
+          <div class="modal-body">
+            <form class="place-update-dynamic" data-id=${place._id}>
+              <input type="text" name="place[localName]" placeholder="New name" required>
+              <input type="text" name="place[country]" placeholder="New local Name" required>
+              <input type="text" name="place[description]" placeholder="New description" required>
+              <input type="text" name="place[surfLevel]" placeholder="surf Level" required>
+              <input type="text" name="place[avgCostDay]" placeholder="New avgCostDay" required>
+              <input type="text" name="place[waveRange]" placeholder="New waveRange" required>
+              <button type="submit" class="index-dynamic-button btn btn-primary" data-dismiss="modal">Update
+                Place</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+    `;
+  });
+
+  $("#placeList").html(storeHTML);
   $("#success-message")
     .text("Your places have been successfully displayed!")
     .addClass(".success");
@@ -106,23 +121,21 @@ const showPlaceSuccess = function (response) {
     <p>Place ID: ${response.place._id}</p>
     <p>Local Name: ${response.place.localName}</p>
     <p>Country: ${response.place.country}</p>
-    <img src="${response.place.localImg}" alt="" width="auto" height="300">
     <p>Description: ${response.place.description}</p>
-    <p>surfLevel: ${response.place.surfLevel}</p>
+    <p>Surf Level: ${response.place.surfLevel}</p>
     <p>Average Cost Day: US$${response.place.avgCostDay}</p>
     <p>Wave Range: ${response.place.waveRange}ft</p>
-    <p>Great for: ${response.place.stance}</p>
   </div>
-  <div id="displayFeedbackId"> 
+  
+  <div id="displayFeedbackId" class="col-12 d-none d-print-block d-flex justify-content-center bg-warning"> 
     <p>Your places have been successfully show!</p>
   </div>
+
   `);
   setTimeout(function () {
     $("#displayFeedbackId").text("").addClass("success");
   }, 4000);
-
 };
-
 
 const showPlaceFailure = (response) => {
   $("#error-message")
